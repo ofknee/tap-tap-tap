8b2704eb3a6df6d90ffde1de24c3f3cd35f75de4
crystal <cheesedrone@gmail.com>
2026-07-20 14:39:35 -0700

    scrolling tileset

---
+++ scenes/iso/jump_strip.gd
@@ -0,0 +1,16 @@
+extends TileMapLayer
+var speed : float = 1
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var filled_tiles := get_used_cells() 
+	for tile: Vector2i in filled_tiles:
+		var neighbor_tiles := get_surrounding_cells(tile)
+		for neighbor: Vector2i in neighbor_tiles: 
+			if get_cell_source_id(neighbor) == -1: # -1 = empty
+				set_cell(neighbor, 0, Vector2i(9,0))
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position -= Vector2(speed*2, -speed)
