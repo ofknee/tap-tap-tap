33f4c1b618836eae3f1f52fdf113abde3f05538b
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-21 09:38:46 -0700

    physics and jump updates

--- scenes/iso/player.gd
+++ scenes/iso/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -430.0
+const SPEED = 180.0
+const JUMP_VELOCITY = -280.0
 @onready var ray = $RayCast2D
 @onready var shadow = $Shadow
 
@@ -18,10 +18,10 @@ func _physics_process(delta: float) -> void:
 		shadow.hide()
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * 1.1
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
