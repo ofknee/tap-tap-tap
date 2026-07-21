4e20112a3a0f6f952e436149b599c957a4a01027
crystal <cheesedrone@gmail.com>
2026-07-21 11:46:21 -0700

    instantiation of spawnables

--- scenes/factory/spawner.gd
+++ scenes/factory/spawner.gd
@@ -1,15 +1,29 @@
 extends Node3D
 
-@export var large_box : Node3D
-@export var small_box : Node3D
-@export var button : Node3D
+@export var large_box : PackedScene
+@export var small_box : PackedScene
+@export var yellow_button : PackedScene
 @onready var spawnpoint =  $Spawnpoint
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.spawn_new.connect(spawn_new)
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn_new(type : int):
-	pass
+	var inst : Node3D
+	match type:
+		0:
+			inst = large_box.instantiate()
+		1:
+			inst = yellow_button.instantiate()
+	add_child(inst)
+	inst.position = spawnpoint.position
+
+
+func _input(event : InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		spawn_new(0)
+	if Input.is_action_just_pressed("LEFT"):
+		spawn_new(1)
