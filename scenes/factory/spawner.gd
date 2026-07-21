7880d597f3069fa8ec22e57ffbfbfb0741234e33
crystal <cheesedrone@gmail.com>
2026-07-21 11:21:05 -0700

    connect signalbus spawner

--- scenes/factory/spawner.gd
+++ scenes/factory/spawner.gd
@@ -7,9 +7,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	SignalBus.spawn_new.connect(spawn_new)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func spawn_new(type : int):
 	pass
