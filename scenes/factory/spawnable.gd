f942f9b6fdc8ba09d0aacc018be0170b7ee03864
crystal <cheesedrone@gmail.com>
2026-07-21 12:08:40 -0700

    generation up and runnning!!!! + random rotation

--- scenes/factory/spawnable.gd
+++ scenes/factory/spawnable.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
-var speed = Global.speed
+var speed = Global.speed/70
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	show()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scenes/factory/spawner.gd
+++ scenes/factory/spawner.gd
@@ -16,17 +16,20 @@ func spawn_new(type : int):
 	var inst : Node3D
 	match type:
 		0:
-			print("i got called :(")
-			inst = large_box.instantiate()
+			if randi_range(0,1) == 0:
+				inst = large_box.instantiate()
+			else:
+				inst = large_box.instantiate()
+			inst.rotation.y = deg_to_rad(randf_range(0.0, 360.0))
 		1:
 			inst = yellow_button.instantiate()
+	
 	add_child(inst)
 	inst.global_position = spawnpoint.global_position
 
 
 func _input(event : InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		print("space pressed")
 		spawn_new(0)
 	if Input.is_action_just_pressed("LEFT"):
 		spawn_new(1)
