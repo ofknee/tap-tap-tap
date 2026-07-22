e9621fd51457829afd8a70bdf32c076f2ea510fb
crystal <cheesedrone@gmail.com>
2026-07-22 11:31:27 -0700

    yellow button and adjust spawnables/collisions

---
+++ scenes/factory/spawnables/yellow_button.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+var speed = Global.speed/70
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	show()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= speed
+
+
+func _on_rigid_body_3d_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
+		print("i got hit by", body.name)
