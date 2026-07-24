e494379cd0e440ebb92527cff8c38f07bab8f80a
Daphne-CPM08 <drobert6@ucsc.edu>
2026-07-24 14:02:10 -0700

    any key pressed

--- scenes/iso/player.gd
+++ scenes/iso/player.gd
@@ -28,9 +28,10 @@ func _physics_process(delta: float) -> void:
 		#else:
 		velocity += get_gravity() * delta * 1.1
 	# Handle jump.
-	if Input.is_action_pressed("ui_accept") and is_on_floor():
+	if Input.is_anything_pressed() and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		SignalBus.tap.emit()
+	
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
