cf3969b5f659dfbd1d1b7a2dc74c53f38d07d5c5
crystal <cheesedrone@gmail.com>
2026-07-26 14:37:10 -0700

    sync & iso collision tweaks

--- scenes/factory/spawnable.gd
+++ scenes/factory/spawnable.gd
@@ -1,6 +1,6 @@
 extends RigidBody3D
 
-var speed = Global.speed/110
+var speed = Global.speed/108.5
 #@onready var top : CollisionShape3D = $Top
 
 # Called when the node enters the scene tree for the first time.
